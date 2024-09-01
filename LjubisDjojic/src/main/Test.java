69dfa67abc617dadafda3fca3d480ff9d96e6e00
Ljubisa Djojic <50331420+AsibuRS@users.noreply.github.com>
2024-09-01 19:10:32 +0200

    Add files via upload

---
+++ LjubisDjojic/src/main/Test.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.List;
+
+import crud.Crud;
+import model.Autor;
+
+/*
+ * Test klasa
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		Crud crud = new Crud();
+		List<Autor> autori = crud.sviAutori();
+		for (Autor a : autori) {
+			System.out.println(a);
+			a.getKnjige().stream().forEach(System.out::println);
+		}
+		System.exit(0);
+	}
+
+}
